7e50ff63c11d8fae403e2069cbbc67c56b45eae3
Hamza Dursun <hamzadursun2507@gmail.com>
2026-09-15 22:12:28 +0300

    ilk commit denemesi

---
+++ 01_hello_world/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //input-output kütüphanesini dahil et
+
+int main() { //main fonksiyonu programın başlangıç noktasıdır
+    printf("Hello, World!\n"); // printf fonk ile ekrana yazı yazdırdık
+    return 0; // program başarılı bir şekilde tamamlandı
+}
+
+// \n karakteriyle yeni bir satır başlattık 
