9b1c05b4eb2cad93fa1f739cd5485eb8e4c64dc1
Hamza Dursun <hamzadursun2507@gmail.com>
2026-09-15 22:16:18 +0300

    dosya guncellemesi

--- 01_hello_world/main.c
+++ 01_hello_world/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h> //input-output kütüphanesini dahil et
 
 int main() { //main fonksiyonu programın başlangıç noktasıdır
-    printf("Hello, World!\n"); // printf fonk ile ekrana yazı yazdırdık
+    printf("Hello, World!\n"); // printf fonksiyonu ile ekrana yazı yazdırdık
     return 0; // program başarılı bir şekilde tamamlandı
 }
 
